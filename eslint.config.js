1c2e23b774bac555c4b939d0930af7fb9ca2ad02
Ludiamen <maxvas1001@gmail.com>
2024-04-14 18:21:37 +0300

    fix eslint

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+export default [
+  // ...other config
+  {
+    ignores: [
+      "**/temp.js",
+      "config/*",
+      "dist/*",
+      "coverage/*",
+      "doc/*",
+      "webpack.*.js",
+    ],
+  },
+];
